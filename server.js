7cc299d145271891e32b93952dc19e183e85ed61
rmakey <rmakey@myseneca.ca>
2023-05-31 23:31:15 -0400

    server.js updated

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+const path = require("path");
+
+var app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+//welcome message
+function onHTTPSTART() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+//set up route to my default page
+app.get("/", (req, res) => {
+  res.redirect("/about");
+});
+
+//set up route to my about page
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+app.listen(HTTP_PORT, onHTTPSTART);
