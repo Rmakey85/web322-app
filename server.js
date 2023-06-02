6a0b8c7c74b11b135f9e5852710b3be1971d10e4
rmakey <rmakey@myseneca.ca>
2023-06-01 23:34:46 -0400

    routes added

--- server.js
+++ server.js
@@ -23,4 +23,26 @@ app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/about.html"));
 });
 
+
+//set up route to shop page
+app.get("/shop", (req, res) => {
+  res.send("TODO: get all items who have published==true");
+});
+
+//set up route to items page
+app.get("/items", (req, res) => {
+  res.send("TODO: get all items");
+});
+
+//set up route to categories page
+app.get("/categories", (req, res) => {
+  res.send("TODO: get all categories");
+});
+
+//set up for no matching route
+app.use((req, res) => {
+  res.status(404).send("Page Not Found" );
+});
+
 app.listen(HTTP_PORT, onHTTPSTART);
+
