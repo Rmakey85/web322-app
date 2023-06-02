a23d0115a23cb7272ab8d031e2cd10297dbc49e3
rmakey <rmakey@myseneca.ca>
2023-06-02 00:17:31 -0400

    data loading functions addded

---
+++ store-service.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+let items = [];
+let categories = [];
+
+module.exports.initialize = function(){
+    return new Promise((resolve,reject)=>{
+        fs.readFile('./data/items.json',(err,data)=>{
+            if(err){
+                console.log(err);
+                reject("unable to read file items.json");
+            }else{
+                items = JSON.parse(data);
+
+                fs.readFile('./data/categories.json',(err,data)=>{
+                    if(err){
+                        console.log(err);
+                        reject("unable to read file categories.json");
+                    }else{
+                        categories = JSON.parse(data);
+                        resolve(true);
+                    }
+                });
+
+            }
+        })
+    })
+}
+
+module.exports.getAllItems = function(){
+    return new Promise((resolve,reject)=>{
+        if(items.length==0){
+            reject("no results returned");
+        }else{
+            resolve(items);
+        }
+    });
+}
+
+module.exports.getPublishedItems = function(){
+    return new Promise((resolve,reject)=>{
+        if(items.length==0){
+            reject("no results returned");
+        }else{
+            let publishedItems = [];
+            items.forEach(value=>{
+                if(value.published==true){
+                    publishedItems.push(value);
+                }
+            });
+            resolve(publishedItems);
+        }
+    })
+}
+
+module.exports.getCategories = function(){
+    return new Promise((resolve,reject)=>{
+        if(categories.length == 0){
+            reject("no results returned")
+        }else{
+            resolve(categories);
+        }
+    });
+}
